acf0742f33362ff4c0a0009aac21fe0db1146756
跨性别 <cvyl@pm.me>
2024-06-09 18:20:03 +0200

    feat: count api route

---
+++ src/app/api/metadata/count/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@/lib/prisma';
+
+export async function GET(req: NextRequest) {
+    try {
+        const url = new URL(req.url);
+        const alias = url.searchParams.get('q');
+
+        if (!alias) {
+            return NextResponse.json({ error: 'Alias parameter is required' }, { status: 400 });
+        }
+
+        // Find the short URL using the alias
+        const short = await prisma.shortURL.findUnique({
+            where: { alias },
+        });
+
+        if (!short) {
+            return NextResponse.json({ error: 'Not Found' }, { status: 404 });
+        }
+
+        // Redirect to the long URL
+        return NextResponse.json(short.userClicks);
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+}
