171e7eb2a402dd438c9058365535b23ae3c82e41
跨性别 <cvyl@pm.me>
2024-06-09 18:27:05 +0200

    fix: dynamic call

--- src/app/api/metadata/count/route.ts
+++ src/app/api/metadata/count/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(req: NextRequest) {
     try {
         const url = new URL(req.url);
