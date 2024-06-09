7091e073a9ffbb24fd6543744f0c0544855c043e
跨性别 <cvyl@pm.me>
2024-06-09 18:02:24 +0200

    feat: user click counter

--- src/app/[alias]/page.tsx
+++ src/app/[alias]/page.tsx
@@ -1,18 +1,33 @@
 import { redirect } from 'next/navigation';
 import prisma from '@/lib/prisma';
 
+export async function counter(alias: string) {
+  const shortURL = await prisma.shortURL.findFirst({
+    where: { alias },
+  });
+
+  if (!shortURL) return null;
+
+  await prisma.shortURL.update({
+    where: { id: shortURL.id },
+    data: { userClicks: shortURL.userClicks + 1 },
+  });
+
+  return shortURL.longURL;
+}
+
 export default async function RedirectPage({
   params,
 }: {
   params: { alias: string };
 }) {
   const { alias } = params;
+  const longURL = await counter(alias);
 
-  const shortURL = await prisma.shortURL.findFirst({
-    where: { alias },
-  });
+  if (!longURL) {
+    redirect('/');
+    return;
+  }
 
-  if (!shortURL) { redirect('/'); }
-
-  redirect(shortURL.longURL);
+  redirect(longURL);
 }
