3c5acf9c59bc60cc2e00c3ef6158402289b518ad
跨性别 <cvyl@pm.me>
2024-06-09 16:03:29 +0200

    fix: no empty strings allowed

--- src/schemas/urlSchema.ts
+++ src/schemas/urlSchema.ts
@@ -2,6 +2,8 @@ import { z } from 'zod';
 
 const urlSchema = z.object({
     slug: z.string()
+    .trim()
+    .min(1, 'Slug must be at least 1 character long')
     .refine(s => !s.includes(' '), 'Slug cannot contain spaces')
     .refine(s => s.length < 100, 'Slug must be less than 100 characters')
     .refine(s => s.length > 0, 'Slug must be at least 1 character long')
